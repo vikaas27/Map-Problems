86e4cfcbcee5ab68604e582639162ff87d91be6a
Vikas Rathod <rathodvikas270506@gmail.com>
2025-03-31 11:34:55 +0530

    Add files via upload

---
+++ two-sum.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+#include <vector>
+#include <unordered_map>
+using namespace std;
+
+class Solution {
+public:
+    vector<int> twoSum(vector<int>& nums, int target) {
+        unordered_map<int, int> numToIndex;
+        for (int i = 0; i < nums.size(); i++) {
+            int complement = target - nums[i];
+            if (numToIndex.find(complement) != numToIndex.end()) {
+                return {numToIndex[complement], i};
+            }
+            numToIndex[nums[i]] = i;
+        }
+        return {}; 
+    }
+};
+
